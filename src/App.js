9cffb5501769e5ed6f2ca8d53f6eb7759b990e1b
sam <syedsamer456@gmail.com>
2021-08-20 13:47:48 +0530

    add cartitem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import CartItem from "./CartItem";
+
 function App() {
   return (
     <div className="App">
       <h1>Kya bolti tu?</h1>
+      < CartItem />
     </div>
   );
 }
+
+
 
 export default App;
